bb51d598ced32adaa102451b51e6ee0623e4f88c
Jesse Waddell <jesse@Jesses-MacBook-Pro.local>
2017-01-31 11:21:12 -0600

    added functionality for saving photo to dom, need to add flexability for ANY photo

--- app/app.js
+++ app/app.js
@@ -2,6 +2,16 @@ console.log('Ayy foo')
 
 const app = angular.module('pictureApp', ['ngRoute'])
 
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyD1CCQcEpt_0yOKRj_Kc1B-Sg_mIfc69P0",
+    authDomain: "storing-photo-with-fb-jwaddell.firebaseapp.com",
+    databaseURL: "https://storing-photo-with-fb-jwaddell.firebaseio.com",
+    storageBucket: "storing-photo-with-fb-jwaddell.appspot.com",
+    messagingSenderId: "774182511424"
+  };
+  firebase.initializeApp(config);
+
 app.config(($routeProvider, $locationProvider) => {
     $locationProvider.hashPrefix('')
     $routeProvider
--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,3 +1,27 @@
 app.controller('HomeCtrl', function() {
     console.log('HomeCtrl')
-})
+
+// Create a root reference
+let storageRef = firebase.storage().ref();
+
+let inputElement = document.getElementById('fileInput');
+inputElement.addEventListener("change", handleFiles, false);
+
+    function handleFiles() {
+        var fileList = this.files;
+        console.log("filelist[0]", fileList[0])
+        storageRef.child(fileList[0].name).put(fileList[0])
+            .then(function(snapshot) {
+                console.log('Uploaded a file (:')
+            })
+    }
+// getting the url
+    storageRef.child('kirby8bit2.jpeg').getDownloadURL()
+    .then(function(url) {
+        var img = document.getElementById('myimg');
+        img.src = url;
+    })
+        .catch(function(error) {
+
+        })
+});
